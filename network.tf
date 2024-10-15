ab56d02a12c2df2b3e42a6fd507f899de174da36
EC2 Default User <ec2-user@ip-172-31-34-249.ec2.internal>
2024-10-15 23:25:21 +0000

    add routes for zone b

--- network.tf
+++ network.tf
@@ -121,12 +121,22 @@ resource "aws_route_table" "private_rtbl_1a" {
 #################################
 ### ROUTE TABLE ASSOCIATION BLOCK
 #################################
-resource "aws_route_table_association" "public_rtbl_associate" {
+resource "aws_route_table_association" "public_rtbl_associate_1a" {
   subnet_id      = aws_subnet.public_subnet_1a.id
   route_table_id = aws_route_table.public_rtbl.id
 }
 
-resource "aws_route_table_association" "private_rtbl_1a_associate" {
+resource "aws_route_table_association" "private_rtbl_associate_1a" {
   subnet_id      = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.private_rtbl_1a.id
+}
+
+resource "aws_route_table_association" "public_rtbl_associate_1b" {
+  subnet_id      = aws_subnet.public_subnet_1b.id
+  route_table_id = aws_route_table.public_rtbl.id
+}
+
+resource "aws_route_table_association" "private_rtbl_associate_1b" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
   route_table_id = aws_route_table.private_rtbl_1a.id
 }
