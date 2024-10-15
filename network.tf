1c52f939e914fbdffbc043e2d89eae8fddaf2931
EC2 Default User <ec2-user@ip-172-31-34-249.ec2.internal>
2024-10-15 23:49:10 +0000

    add zone b routing

--- network.tf
+++ network.tf
@@ -137,6 +137,19 @@ resource "aws_route_table" "private_rtbl_1a" {
   }
 }
 
+resource "aws_route_table" "private_rtbl_1b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_1b.id
+  }
+
+  tags = {
+    Name = "tf_private_1b_rtbl"
+  }
+}
+
 #################################
 ### ROUTE TABLE ASSOCIATION BLOCK
 #################################
